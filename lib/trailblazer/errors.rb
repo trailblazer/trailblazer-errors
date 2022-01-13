98129278519be437b15a28060b256cfa83bff6f2
Nick Sutterer <apotonick@gmail.com>
2022-01-13 12:33:41 +0100

    first commit into an unsuspecting world.

---
+++ lib/trailblazer/errors.rb
@@ -0,0 +1,7 @@
+require_relative "errors/version"
+
+module Trailblazer
+  module Errors
+    # Your code goes here...
+  end
+end
---
+++ lib/trailblazer/errors/version.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module Trailblazer
+  module Errors
+    VERSION = "0.0.1"
+  end
+end
